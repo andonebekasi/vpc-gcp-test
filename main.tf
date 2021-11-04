64dcc2190b54eaa17f04f06b00cc9edb1f2956af
andi <34510976+andonebekasi@users.noreply.github.com>
2021-11-04 15:24:49 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,15 @@
-provider “google” {
-
-project = “poc-danamon-devsecops”
-
-region = “asia-southeast2”
-
+resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges" {
+  name          = "test-subnetwork"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = "asia-southeast2"
+  network       = google_compute_network.custom-test.id
+  secondary_ip_range {
+    range_name    = "tf-test-secondary-range-update1"
+    ip_cidr_range = "192.168.10.0/24"
+  }
 }
 
-resource “google-compute_network” “vpc_network” {
-
-name = “test_terraform_vpc”
-
+resource "google_compute_network" "custom-test" {
+  name                    = "test-network"
+  auto_create_subnetworks = false
 }
